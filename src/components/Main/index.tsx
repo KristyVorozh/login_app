97c2723196b9ccabc8cc53c2cbe960b11edd961a
kristina <kristina.vorojejkina@yandex.ru>
2022-06-06 16:23:13 +0700

    create app

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import './style.sass';
+import Logo from './img/logo.svg';
+import InputMask from "react-input-mask";
+import ModalOpen from "../Modal";
+
+const Main = () => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [status, setStatus] = useState('');
+    const [recovery, setRecovery] = useState(false);
+    const [modal, setModal] = useState(false);
+
+    const signIn = () => {
+      if (login === '+7-111-111-11-11' && password === '123456') setStatus('Успешно')
+      else setStatus('Логин/пароль не верный')
+    }
+
+    return (
+        <div className='main'>
+            <div className="main_section">
+                <img src={Logo} alt=""/>
+                {!recovery ?
+                    <>
+                        <div className="main_input">
+                            <div className="main_input-content">
+                                <p>Введите логин</p>
+                                <InputMask mask="+9-999-999-99-99" value={login || ''} onChange={(e: any)=>setLogin(e.target.value)}/>
+                            </div>
+                            <div className="main_input-content">
+                                <p>Введите пароль</p>
+                                <input value={password || ''} onChange={(e) => setPassword(e.target.value)} type="password"/>
+                            </div>
+                        </div>
+                        <div onClick={()=>setRecovery(true)} className="main_recovery">
+                            Забыли пароль?
+                        </div>
+                        {status &&
+                            <p>{status}</p>
+                        }
+                        <button onClick={signIn}>
+                            ВОЙТИ
+                        </button>
+                    </>
+                    :
+                    <>
+                        {modal &&
+                            <ModalOpen modal={modal} login={login} setModal={setModal} setRecovery={setRecovery} setLogin={setLogin}/>
+                        }
+                        <div className='main_recovery-title'>Восстановление пароля</div>
+                        <div className="main_input-content">
+                            <p>Введите номер телефона</p>
+                            <InputMask mask="+9-999-999-99-99" value={login || ''} onChange={(e)=>setLogin(e.target.value)}/>
+                        </div>
+                        <div className='main_callback' onClick={()=>setRecovery(false)}>назад</div>
+                        <button onClick={()=>setModal(true)}>
+                            ПОЗВОНИТЬ
+                        </button>
+                    </>
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Modal from 'react-modal';
+import '../Main/style.sass';
+
+interface IModal {
+    modal: boolean
+    setModal: any
+    setRecovery: any
+    setLogin: any
+    login: string
+}
+const ModalOpen: React.FC <IModal> = ({modal, setModal, setRecovery, setLogin, login}) => {
+    const closeModal = () => {
+        setLogin(login)
+        setModal(false)
+        setRecovery(false)
+    }
+    return (
+        <Modal isOpen={modal} >
+            <p className='close' onClick={closeModal}>X</p>
+            <div>Ваш пароль 123456</div>
+        </Modal>
+    );
+};
+
+export default ModalOpen;
